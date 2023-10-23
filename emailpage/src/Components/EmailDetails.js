64dafaefe029b9135ab6f05f3f8c8ad1ff7dc5cb
Kaushik <kmzdr1@gmail.com>
2023-10-23 19:47:13 +0530

    emaildetailsBody styling; Added add to favourites;

--- emailpage/src/Components/EmailDetails.js
+++ emailpage/src/Components/EmailDetails.js
@@ -1,6 +1,10 @@
 import React from 'react'
+import { useSelector } from 'react-redux';
 
 const EmailDetails = () => {
+  // const emailBodyDetails=useSelector(store=>store.app.emailDetails)
+  const [emailBody,setEmailBody]=React.useState([])
+
   React.useEffect(()=>{
     emailDescription();
 
@@ -8,18 +12,20 @@ const EmailDetails = () => {
 
   const emailDescription=async()=>{
     try{
-      const data=await fetch("https://flipkart-email-mock.vercel.app/?id=3")
+      const data=await fetch("https://flipkart-email-mock.vercel.app/?id=2")
       if(!data.ok){
         throw new Error("There is an issue with the Network response")
       }
       const json=await data.json()
+      setEmailBody(json)
+      console.log(emailBody)
     }catch(error){
       console.error("Error:",error)
     }
   }
   return (
     <div className="border custom-border rounded-lg m-4 h-screen">
-      <h1>I am back</h1>
+      <h1>{emailBody}</h1>
 
     </div>
   )
